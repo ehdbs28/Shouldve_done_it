40bae6c1985f80e951bdd0263f5b902bf536bbc3
JUNSUNG06 <forest2847@gmail.com>
2024-11-23 19:08:11 +0900

    import room animation

--- Assets/01.Scripts/Scene/Scene_Room.cs
+++ Assets/01.Scripts/Scene/Scene_Room.cs
@@ -4,15 +4,5 @@ using UnityEngine;
 
 public class Scene_Room : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
