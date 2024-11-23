b6954b8d273ed8cbc5d1db7cc7d630fe91f0b283
2m8 <doyun7731@gmail.com>
2024-11-24 06:54:50 +0900

    시츄에이션 선택 연결

--- Assets/01.Scripts/UI/Intro/SituationSlotUI.cs
+++ Assets/01.Scripts/UI/Intro/SituationSlotUI.cs
@@ -7,17 +7,6 @@ public class SituationSlotUI : MonoBehaviourUI
     [SerializeField] SituationDataSO currentSituationData = null;
     [SerializeField] float displayDelay = 0f;
 
-    [Space(15f)]
-    [SerializeField] Image thumbnailImage = null;
-    [SerializeField] Text titleText = null;
-
-    private CanvasGroup canvasGroup = null;
-
-    private void Awake()
-    {
-        canvasGroup = GetComponent<CanvasGroup>();
-    }
-
     private void OnEnable()
     {
         Show();
@@ -28,8 +17,8 @@ public class SituationSlotUI : MonoBehaviourUI
         base.Show();
         RefreshUI();
         
-        canvasGroup.alpha = 0f;
-        canvasGroup.DOFade(1f, 0.75f).SetDelay(displayDelay);
+        // canvasGroup.alpha = 0f;
+        // canvasGroup.DOFade(1f, 0.75f).SetDelay(displayDelay);
     }
 
     private void RefreshUI()
@@ -37,8 +26,8 @@ public class SituationSlotUI : MonoBehaviourUI
         if(currentSituationData == null)
             return;
 
-        thumbnailImage.sprite = currentSituationData.situationThumbnail;
-        titleText.text = currentSituationData.situationName;
+        // thumbnailImage.sprite = currentSituationData.situationThumbnail;
+        // titleText.text = currentSituationData.situationName;
 
         if(DataManager.UserData.ProgressData.ClearedSituationList.Contains(currentSituationData.situationName))
             Debug.Log("->Set Dimed");
